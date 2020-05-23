c28a14972a28bc58efb31191b57608fffa1b8592
michaelchun <michael.chun03@gmail.com>
2020-05-23 18:20:42 -0400

    add stub repl

--- impls/python3/step0_repl.py
+++ impls/python3/step0_repl.py
@@ -1,1 +1,16 @@
-print("Hello")
+def read_lisp(s):
+    return s
+
+def eval_lisp(s):
+    return s
+
+def print_lisp(s):
+    return s
+
+def rep(input):
+    return print_lisp(eval_lisp(read_lisp(input)))
+
+while True:
+    prompt = input("user> ")
+    print(prompt)
+    print(rep(prompt))
