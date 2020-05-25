3e615787b9884ec00d6938b139a7f097a4c6d9b6
michaelchun <michael.chun03@gmail.com>
2020-05-24 22:55:40 -0400

    add boilerplate for reader

---
+++ impls/python3/reader.py
@@ -0,0 +1,15 @@
+class Reader:
+    def __init__(self, tokens, position):
+        pass
+    
+    def next(self):
+        pass
+
+    def peek(self):
+        pass
+
+def read_str(str):
+    pass
+
+def tokenize(str):
+    pass
